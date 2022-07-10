66f3fcbadbae5f6f0c9ba8977c70216f1e502830
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2022-07-09 20:32:56 -0600

    change to object oriented programming

--- src/getObject.py
+++ src/getObject.py
@@ -4,22 +4,77 @@ import os
 from PIL import Image
 import time
 
-def getObject(path):
-    def findTag(root, tag):
-        element = 0
-        curTag = ''
+class createObject():
+    def __init__(self,path):
+        with open(path) as file:
+            xml = etree.parse(file)
+        assets = os.path.dirname(os.path.dirname(path))
+        tree = xml.getroot()
 
-        for e in range(len(root)):
-            print(root[e].tag)
-            curTag = root[e].tag
-            if curTag == tag:
-                return e
-                break
+        elmt = findTag(tree, 'Sprites')
+    
+        self.sprites = []
 
-        return e
+        for e in tree[elmt]:
+            sprite = {}
+            sprite['filename'] = e.get('filename')
+            sprite['pos'] = e.get('pos')
+            sprite['angle'] = e.get('angle')
+            sprite['gridSize'] = e.get('gridSize')
+            sprite['isBackground'] = e.get('isBackground') == 'true'
+            sprite['visible'] = e.get('visible') == 'true' or e.get('visible') == None
 
-    def getImage(spritePath):
-        def findInImagelist(path, name):
+            sprite_obj = self.sprite(assets + sprite['filename'], sprite)
+
+            self.sprites.append(sprite_obj)
+
+        self.properties = {}
+        prop = tree[findTag(tree, 'DefaultProperties')]
+        for p in prop:
+            self.properties[p.get('name')] = p.get('value')
+
+    class sprite():
+        def __init__(self, path, properties) -> None:
+            self.assets = os.path.dirname(os.path.dirname(path))
+            with open(path) as file:
+                xml = etree.parse(file)
+                tree = xml.getroot()
+
+            print(len(tree))
+            print(tree.tag)
+            self.animations = []
+            for animation_element in tree:
+                print(animation_element)
+
+                info = {}
+
+                info['name'] = animation_element.get('name')
+                info['textureBasePath'] = animation_element.get('textureBasePath')
+                info['atlas'] = animation_element.get('atlas')
+                info['fps'] = animation_element.get('fps')
+                info['playbackMode'] = animation_element.get('playbackMode')
+                info['loopCount'] = animation_element.get('loopCount')
+
+                frames = []
+
+                for f in animation_element:
+                    frame = {}
+                    frame['name'] = f.get('name')
+                    frames.append(frame)
+
+                for f in range(len(frames)):
+                    frames[f] = self.findInImagelist(self.assets + info['atlas'], frames[f]['name'])
+
+                self.animations.append(self.animation(frames, info))
+
+                self.filename = properties['filename'],
+                self.pos = properties['pos'],
+                self.angle = properties['angle'],
+                self.gridSize = properties['gridSize'],
+                self.isBackground = properties['isBackground'],
+                self.visible = properties['visible']
+        
+        def findInImagelist(self, path, name):
             with open(path) as file:
                 xml = etree.parse(file)
                 tree = xml.getroot()
@@ -44,7 +99,7 @@ def getObject(path):
             image_info['size'] = image.get('size')
             image_info['rect'] = image.get('rect')
 
-            sheet = Image.open(assets + sheet_info['file'])
+            sheet = Image.open(self.assets + sheet_info['file'])
 
             left, up, right, down = image_info['rect'].split()
             left = int(left)
@@ -61,27 +116,30 @@ def getObject(path):
             y = int(y)
             # image = image.resize((x,y))
             
-            image.show()
-            print('done')
+            # image.show()
+            print('got frame ' + image_info['name'])
 
-            # image, name, offset, size, rect
+            return self.frame(image, image_info)
 
-            def constructor(self, image, offset, size, rect):
+        class frame():
+            def __init__(self, image, properties) -> None:
+                self.name = properties['name']
+
                 self.image = image
 
-                offset = offset.split()
+                offset = properties['offset'].split()
                 x = int(offset[0])
                 y = int(offset[1])
 
                 self.offset = [x,y]
 
-                newsize = size.split()
+                newsize = properties['size'].split()
                 w = int(newsize[0])
                 h = int(newsize[1])
 
                 self.size = [w,h]
 
-                left, up, right, down = rect.split()
+                left, up, right, down = properties['rect'].split()
                 left = int(left)
                 up = int(up)
                 right = int(right)
@@ -91,92 +149,29 @@ def getObject(path):
 
                 self.rect = [left, up, right, down]
 
-            frame = type("frame", (object, ), {
-                # constructor
-                "__init__": constructor,
-                "name": image_info['name']
-            })
-
-            return frame(image, image_info['offset'], image_info['size'], image_info['rect'])
-
-        # assets = os.path.dirname(os.path.dirname(spritePath))
-        
-        with open(spritePath) as file:
-            xml = etree.parse(file)
-            tree = xml.getroot()
-        
-        print(len(tree))
-        print(tree.tag)
-        animations = []
-        for animation_element in tree:
-            print(animation_element)
-                
-            info = {}
-
-            info['name'] = animation_element.get('name')
-            info['textureBasePath'] = animation_element.get('textureBasePath')
-            info['atlas'] = animation_element.get('atlas')
-            info['fps'] = animation_element.get('fps')
-            info['playbackMode'] = animation_element.get('playbackMode')
-            info['loopCount'] = animation_element.get('loopCount')
-
-            frames = []
-
-            for f in animation_element:
-                frame = {}
-                frame['name'] = f.get('name')
-                frames.append(frame)
-
-            for f in range(len(frames)):
-                frames[f] = findInImagelist(assets + info['atlas'], frames[f]['name'])
-
-            def constructor(self, name, frames):
-                self.name = name
+        class animation():
+            def __init__(self, frames, properties) -> None:
                 self.frames = frames
-
-            animation = type("animation", (object, ), {
-                # constructor
-                "__init__": constructor,
-                "textureBasePath": info['textureBasePath'],
-                "atlas": info['atlas'],
-                "fps": info['fps'],
-                "playbackMode": info['playbackMode'],
-                "loopCount": info['loopCount']
-            })
-
-            animations.append(animation(info['name'],frames))
-
-        return animations
-
-        
+                self.name = properties['name']
+                self.textureBasePath = properties['textureBasePath']
+                self.atlas = properties['atlas']
+                self.fps = properties['fps']
+                self.playbackMode = properties['playbackMode']
+                self.loopCount = properties['loopCount']
 
 
-    with open(path) as file:
-        xml = etree.parse(file)
-    assets = os.path.dirname(os.path.dirname(path))
-    tree = xml.getroot()
 
-    # image, properies, name, filename
+                
 
-    elmt = findTag(tree, 'Sprites')
-    
-    sprites = []
+def findTag(root, tag):
+    element = 0
+    curTag = ''
+    for e in range(len(root)):
+        print(root[e].tag)
+        curTag = root[e].tag
+        if curTag == tag:
+            return e
+            break
 
-    for e in tree[elmt]:
-        sprite = {}
-        sprite['filename'] = e.get('filename')
-        sprite['pos'] = e.get('pos')
-        sprite['angle'] = e.get('angle')
-        sprite['gridsize'] = e.get('gridsize')
-        sprite['isBackground'] = e.get('isBackground') == 'true'
-
-        sprites.append(sprite)
-
-    for s in range(len(sprites)):
-        sprites[s] = getImage(assets + sprites[s]['filename'])
-
-    return sprites
-
-    
 if __name__ == '__main__':
-    print(getObject('game/wmw/assets/Objects/balloon.hs'))
+    print(createObject('game/wmw/assets/Objects/balloon.hs'))
