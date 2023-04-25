7985d3aba1c47a080db579020fcbf3cc23f1420b
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-04-24 21:07:56 -0600

    Delete readxml.py

--- src/readxml.py
+++
@@ -1,9 +0,0 @@
-import lxml
-from lxml import etree
-
-with open('game/wmw/assets/Objects/shower_head.hs') as file:
-    xml = etree.parse(file)
-
-root = xml.getroot()
-
-print(root[1][0][0].get('test'))
