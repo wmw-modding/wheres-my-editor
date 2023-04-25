b27cdb52429661aa857bc49172027d903bcf8001
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-04-24 21:07:47 -0600

    remove unnecessary popups

--- src/popups.py
+++ src/popups.py
@@ -1,23 +1,8 @@
 import copy
 import json
-import threading
 import tkinter as tk
 from tkinter import StringVar, ttk, filedialog
 import os
-
-def commingsoon():
-    popup = tk.Toplevel()
-    popup.wm_title('.')
-    ttk.Label(popup, text="Comming Soon maybe").pack()
-    ttk.Button(popup, text="OK", command = popup.destroy ).pack()
-       
-
-def about_dialog():
-    popup = tk.Toplevel()
-    popup.wm_title('About')
-    ttk.Label(popup, text="Hai").pack()
-    ttk.Label(popup, text="Enjoying the program?").pack()
-    ttk.Button(popup, text="Eh", command = popup.destroy ).pack()
 
 class load_dialog():
     def __init__(self, root=None, max=280) -> None:
