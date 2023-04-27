e4c1e9447f80544f6230a5d78845cfcb30a77d61
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-04-26 19:25:40 -0600

    make properties editable
    I now just need to add scrolling, and fix up selecting objects, and dragging objects.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import tkinter.filedialog as filedialog
 import tkinter as tk
 from tkinter import ttk, simpledialog, messagebox
+import tkwidgets
 import popups
 from PIL import Image, ImageTk
 import json
@@ -9,6 +10,7 @@ import os
 from settings import Settings
 from lxml import etree
 import numpy
+import typing
 
 import wmwpy
 
@@ -46,7 +48,7 @@ class WME(tk.Tk):
 
         self.active = True
 
-        self.currentObj = None
+        self.selectedObject = None
         self.level : wmwpy.classes.Level = None
         self.game : wmwpy.Game = None
         
@@ -59,7 +61,6 @@ class WME(tk.Tk):
             self.settings.get('game.default_level.xml'),
             self.settings.get('game.default_level.image')
         )
-        self.updateLevel()
 
     def createWindow(self):
         self.seperator = ttk.PanedWindow(orient='horizontal')
@@ -70,9 +71,11 @@ class WME(tk.Tk):
 
         self.objects_frame = ttk.Frame(self.side_pane, width=200, height=300)
         self.side_pane.add(self.objects_frame)
-
-        self.properties_frame = ttk.Frame(self.side_pane, width=200, height=300)
-        self.side_pane.add(self.properties_frame)
+        
+        self.properties = {
+            'frame' : ttk.Frame(self.side_pane, width=200, height=300)
+        }
+        self.side_pane.add(self.properties['frame'])
 
         self.level_canvas = tk.Canvas(self.seperator, width=90*self.scale, height=120*self.scale)
         self.seperator.add(self.level_canvas)
@@ -83,6 +86,8 @@ class WME(tk.Tk):
         ),
             'objects': {}
         }
+        
+        self.resetProperties()
     
     OBJECT_OFFSET = [1.25,-1.3]
     
@@ -99,7 +104,6 @@ class WME(tk.Tk):
             self.level_canvas.itemconfig(
                 id,
                 image = photoImage,
-                anchor = 'c',
             )
             
             self.level_canvas.moveto(
@@ -120,6 +124,153 @@ class WME(tk.Tk):
             '<Button1-Motion>',
             lambda e: self.dragObject(obj, e)
         )
+        self.level_canvas.tag_bind(
+            id,
+            '<ButtonRelease-1>',
+            lambda e: self.selectObject(obj)
+        )
+    
+    def updateProperties(self, obj : wmwpy.classes.Object = None):
+        if obj == None:
+            obj = self.selectedObject
+        
+        self.resetProperties()
+        
+        ROW_SIZE = 25
+        
+        def addProperty(
+            property : str,
+            value : str,
+            type : typing.Literal['number', 'text'] = 'text',
+            removable = True,
+            row = 0,
+            entry_callback : typing.Callable[[str], typing.Any] = None,
+            label_callback : typing.Callable[[str], bool] = None,
+            **kwargs,
+        ):
+            if removable:
+                name = tkwidgets.EditableLabel(
+                    self.properties['left'],
+                    text = property,
+                    callback = label_callback,
+                )
+            else:
+                name = ttk.Label(
+                    self.properties['left'],
+                    text=property,
+                )
+            name.grid(row=row, sticky='we')
+            
+            type = type.lower()
+            
+            if type == 'number':
+                input = ttk.Spinbox(
+                    self.properties['right'],
+                    **kwargs
+                )
+            else:
+                input = ttk.Entry(
+                    self.properties['right'],
+                    **kwargs
+                )
+            
+            input.insert(0, value)
+            
+            input.grid(column = 0, row=row, sticky = 'we')
+            if entry_callback:
+                input.bind('<Return>', lambda e: entry_callback(input.get()))
+                input.bind('<FocusOut>', lambda e: entry_callback(input.get()))
+            
+            if removable:
+                removeButton = ttk.Button(
+                    self.properties['right'],
+                    text='-',
+                    width=2,
+                )
+                removeButton.grid(column=1, row=row)
+            
+            self.properties['left'].rowconfigure(row, minsize=ROW_SIZE)
+            self.properties['right'].rowconfigure(row, minsize=ROW_SIZE)
+    
+        def updateProperty(property, value):
+            obj.properties[property] = value
+            self.updateObject(obj)
+        
+        def updatePropertyName(property, newName):
+            if newName == property:
+                return True
+            print(f'{newName = }')
+            print(f'{property = }')
+            
+            if newName in obj.properties:
+                messagebox.showerror(
+                    'Property name already exists',
+                    f'Property "{newName}" already exists.'
+                )
+                
+                return False
+            else:
+                value = obj.properties[property]
+                del obj.properties[property]
+                obj.properties[newName] = value
+                
+                self.updateObject(obj)
+                self.updateProperties(obj)
+                
+                return True
+        
+        addProperty('Name', obj.name, 'text', removable = False, row=0)
+        angle = '0'
+        if 'Angle' in obj.properties:
+            angle = obj.properties['Angle']
+        addProperty(
+            'Angle',
+            angle,
+            'number',
+            removable = False,
+            row=1,
+            from_=-360,
+            to=360,
+            entry_callback = lambda value: updateProperty('Angle', value),
+        )
+        
+        row = 1
+        
+        for property in obj.properties:
+            if property not in ['Angle']:
+                row += 1
+                addProperty(
+                    property,
+                    obj.properties[property],
+                    row=row,
+                    entry_callback = lambda value, prop = property: updateProperty(prop, value),
+                    label_callback = lambda name, prop = property: updatePropertyName(prop, name)
+                )
+        
+    def resetProperties(self):
+        
+        if not 'panned' in self.properties:
+            self.properties['panned'] = ttk.PanedWindow(
+                self.properties['frame'],
+                orient='horizontal'
+            )
+            self.properties['panned'].pack(expand=True, fill='both')
+        
+        if 'left' in self.properties:
+            for widget in self.properties['left'].winfo_children():
+                widget.destroy()
+        else:
+            self.properties['left'] = ttk.Frame(self.properties['panned'])
+            self.properties['panned'].add(self.properties['left'], weight=2)
+            
+        if 'right' in self.properties:
+            for widget in self.properties['right'].winfo_children():
+                widget.destroy()
+        else:
+            self.properties['right'] = ttk.Frame(self.properties['panned'])
+            self.properties['panned'].add(self.properties['right'], weight=2)
+            self.properties['right'].columnconfigure(0, weight = 4)
+            # self.properties['right'].columnconfigure(1, weight = 1)
     
     def updateLevel(self):
         self.level_canvas.itemconfig(
@@ -140,6 +291,12 @@ class WME(tk.Tk):
         obj.pos = tuple(pos)
         
         self.updateObject(obj)
+    
+    def selectObject(self, obj : wmwpy.classes.Object = None):
+        self.selectedObject = obj
+        print(obj.name)
+        
+        self.updateProperties()
         
     
     def createMenubar(self):
@@ -153,7 +310,23 @@ class WME(tk.Tk):
         self.menubar.add_cascade(label = 'File', menu = self.file_menu)
     
     def openLevel(self, *args):
-        pass
+        xml = filedialog.askopenfilename(
+            defaultextension = '.xml',
+            filetypes = (
+                ('WMW Level', '.xml'),
+                ('Any', '*.*')
+            ),
+            initialdir = wmwpy.Utils.path.joinPath(
+                self.game.gamepath,
+                self.game.assets,
+                self.game.baseassets,
+                'Levels'
+            )
+        )
+        
+        image = os.path.splitext(xml)[0] + '.png'
+        
+        self.loadLevel(xml, image)
     
     def saveLevel(self, *args, filename = None):
         pass
@@ -205,6 +378,7 @@ class WME(tk.Tk):
             print('Unable to load level')
         
         self.level.scale = 5
+        self.updateLevel()
         return self.level
     
     def updateSettings(this):
---
+++ src/tkwidgets.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import ttk
+import typing
+
+class EditableLabel(ttk.Label):
+    def __init__(self, parent, *args, callback : typing.Callable[[str], bool] = None, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.entry = ttk.Entry(self)
+        self.callback = callback
+        self.bind("<Double-1>", self.edit_start)
+        self.entry.bind("<Return>", self.edit_stop)
+        self.entry.bind("<FocusOut>", self.edit_stop)
+        self.entry.bind("<Escape>", self.edit_cancel)
+
+    def edit_start(self, event=None):
+        self.entry.delete(0, "end")
+        text = self.cget("text")
+        self.entry.insert(0, text)
+        self.entry.place(relx=.5, rely=.5, relwidth=1.0, relheight=1.0, anchor="center")
+        self.entry.focus_set()
+        
+        self.editing = True
+
+    def edit_stop(self, event=None):
+        if not self.editing:
+            return
+        self.editing = False
+        
+        text = self.entry.get()
+        result = True
+        if self.callback:
+            result = self.callback(text)
+        
+        if result:
+            try:
+                self.configure(text=text)
+                self.entry.place_forget()
+            except:
+                pass
+        else:
+            self.edit_cancel()
+        
+
+    def edit_cancel(self, event=None):
+        self.entry.delete(0, "end")
+        self.entry.place_forget()
