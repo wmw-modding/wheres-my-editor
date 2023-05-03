66a0dc0f8eed74818c7b8fc0b9083701e1ea3030
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-05-02 21:28:06 -0600

    add debug build option
    This has no effect on windows, but apparently it does on mac

--- build.py
+++ build.py
@@ -1,11 +1,31 @@
 import PyInstaller.__main__
 import os
+import sys
+import time
 
-PyInstaller.__main__.run([
+options = [
     'src/main.pyw',
     '--onefile',
-    '--windowed',
     '--icon=src/assets/images/icon.png',
     f'--add-data=src/assets{os.pathsep}assets',
     '--name=wme',
-])
+]
+
+debug = False
+
+args = sys.argv
+if len(args) > 1:
+    if args[1] in ['-d', '--debug']:
+        debug = True
+        
+if debug:
+    options.append('--console')
+else:
+    options.append('--noconsole')
+
+
+
+print(f'{options = }')
+time.sleep(1)
+
+PyInstaller.__main__.run(options)
