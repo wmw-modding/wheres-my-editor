255e6a12b72cff1903f3e77da427e6c0f87617af
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-05-29 19:33:59 -0600

    use --ondir instead of --onefile

--- build.py
+++ build.py
@@ -2,30 +2,43 @@ import PyInstaller.__main__
 import os
 import sys
 import time
+import platform
+
+print(f'platform: {platform.system()}')
 
 options = [
     'src/main.py',
-    '--onefile',
     '--icon=src/assets/images/icon_256x256.ico',
     f'--add-data=src/assets{os.pathsep}assets',
     '--name=wme',
 ]
 
+# if platform.system() == 'Darwin':
+options.append('--onedir')
+
 debug = False
 
 args = sys.argv
 if len(args) > 1:
-    if args[1] in ['-d', '--debug']:
-        debug = True
+    args = args[1:]
+    
+    for index in range(len(args)):
+        arg = args[index]
+        
+        if arg in ['-d', '--debug']:
+            debug = True
+        elif arg in ['-o', '--output']:
+            options.append(f"--distpath={os.path.join('./dist', args[index + 1])}")
+            options.append(f"--workpath={os.path.join('./build', args[index + 1])}")
         
 if debug:
     options.append('--console')
 else:
-    options.append('--noconsole')
+    options.append('--windowed')
 
 
 
 print(f'{options = }')
-time.sleep(1)
+# time.sleep(1)
 
 PyInstaller.__main__.run(options)
